b992a6f7c3c0775b5d6fe13115e142a938698488
rundel <rundel@linux.site>
2015-01-29 11:02:44 +0100

    counter initializing

--- dumpwrapper/dumpwrapper.cpp
+++ dumpwrapper/dumpwrapper.cpp
@@ -1,11 +1,14 @@
 #include <QString>
 #include "dumpwrapper.h"
 DumpWrapper::DumpWrapper(QTextStream *output, uchar items_per_column, uchar columns_number){
-	out=output;per_column=items_per_column;colunms=columns_number;
+    out=output;per_column=items_per_column;colunms=columns_number;cnt=0;
 }
 DumpWrapper::~DumpWrapper(){}
 DumpWrapper &DumpWrapper::operator <<(uchar value){
-	if(0==cnt%(per_column*colunms)){*out<<QString("\n%1").arg(cnt,4,16,QChar('0'));}
+    if(0==cnt%(per_column*colunms)){
+        if(cnt>0)*out<<"\n";
+        *out<<QString("%1").arg(cnt,4,16,QChar('0'));
+    }
 	if(0==cnt%(per_column))*out<<" ";
 	*out<<QString(" %1").arg(value,2,16,QChar('0'));cnt++;
 }
