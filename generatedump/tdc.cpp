90fd1086ef0bce231415556efcf334eac7965419
alexkernphysiker <kernphysiker@openmailbox.org>
2015-02-06 16:58:33 +0100

    include for bit operations templates (h -> cc! requires numtype to be defined before including)

--- generatedump/tdc.cpp
+++ generatedump/tdc.cpp
@@ -1,21 +1,19 @@
 #include "tdc.h"
-#include "bit_opr.h"
 #include <math.h>
 using namespace std;
 namespace DumpGenerate {
-TDCHeader::TDCHeader(bool error){
-	if(error)
-		m_error=(rand()&bits<15,0,numtype>::set);
-	else
-		m_error=0;
+#include "bit_opr.cc"//define bit operations
+TDCHeader::TDCHeader(bool error) {
+    if(error)
+        m_error=(rand()&bits<15,0>::set);
+    else
+        m_error=0;
 }
-TDCHeader::~TDCHeader(){}
+TDCHeader::~TDCHeader() {}
 const numtype tdc_header_signature=0x20000000;
-numlist TDCHeader::out(){
-	numlist res;
-	res.push_back(m_error|(rand()&bits<24,16,numtype>::set)|bit<29,numtype>::set);
-	return res;
+numlist TDCHeader::out() {
+    numlist res;
+    res.push_back(m_error|(rand()&bits<24,16>::set)|bit<29>::set);
+    return res;
 }
-
-
 }
