d4e374b67e4ec82d3a1ed04cfc0346a4815a371e
alexkernphysiker <kernphysiker@openmailbox.org>
2015-02-06 18:53:58 +0100

    bit opr small fix

--- generatedump/tdc.cpp
+++ generatedump/tdc.cpp
@@ -2,19 +2,19 @@
 #include <math.h>
 using namespace std;
 namespace DumpGenerate {
-	#include "../math_h/bit_opr.cc"//define bit operations
+#include "../math_h/bit_opr.cc"//define bit operations
 numtype TDCHeader(bool error) {
     numtype errorcode=0;
-	numtype somecode=0x77;//it's ignored
+    numtype somecode=0x77;//it's ignored
     if(error)
-        errorcode=rand();
-	return occupy_bits<15,0>(errorcode)|occupy_bits<23,16>(somecode)|(0b001<<29);
+        errorcode=0xffff;
+    return occupy_bits<15,0>(errorcode)|occupy_bits<23,16>(somecode)|bit<29>::set;
 }
-numpair TDCTime(numtype channel, numtype epoch, numtype coasser, numtype finetime,bool rizingedge){
-	numtype rize=0;
-	if(rizingedge)rize=bit<11>::set;
-	return make_pair((0b011<<29)|occupy_bits<27,0>(epoch),
-					 (0b1<<31)|occupy_bits<28,22>(channel)|occupy_bits<21,12>(finetime)|rize|occupy_bits<10,0>(coasser)
-				);
+numpair TDCTime(numtype channel, numtype epoch, numtype coasser, numtype finetime,bool rizingedge) {
+    numtype rize=0;
+    if(rizingedge)rize=bit<11>::set;
+    return make_pair(bits_in<30,29>::set|occupy_bits<27,0>(epoch),
+                     bit<31>::set|occupy_bits<28,22>(channel)|occupy_bits<21,12>(finetime)|rize|occupy_bits<10,0>(coasser)
+                    );
 }
 }
