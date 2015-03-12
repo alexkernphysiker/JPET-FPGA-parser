4db0ed5d123e0b7f145e2f8f8f4ab9249e32b9a7
alexkernphysiker <kernphysiker@openmailbox.org>
2015-03-12 19:34:55 +0100

    parser added to simulation

--- packet_simulation/top.vhd
+++ packet_simulation/top.vhd
@@ -16,28 +16,53 @@ ARCHITECTURE behavior OF top IS
          debug_out : OUT  std_logic_vector(127 downto 0)
         );
     END COMPONENT;
-    
+    component parser
+	 Port ( 
+	        clk_read : in  STD_LOGIC;
+           clk_parse : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           data_valid : in  STD_LOGIC;
+			  data_in : in STD_LOGIC_VECTOR(7 downto 0);
+           start_packet : in  STD_LOGIC;
+           end_packet : in  STD_LOGIC;			  
+			  debug_output: out STD_LOGIC_VECTOR (127 downto 0)			  
+			);
+	 end component;
 
    signal clock : std_logic := '0';
+   signal clock_parse : std_logic := '0';
+   signal reset : std_logic := '0';
 
    signal data_valid : std_logic;
-   signal data_out : std_logic_vector(7 downto 0);
+   signal data_bus : std_logic_vector(7 downto 0);
    signal start_packet : std_logic;
    signal end_packet : std_logic;
    signal debug_out : std_logic_vector(127 downto 0);
+   signal debug_out_parser : std_logic_vector(127 downto 0);
 
    constant clock_period : time := 10 ns;
+   constant clock_parser_period : time := 10 ns;
  
 BEGIN
  
    uut: packet_simulation PORT MAP (
           clock => clock,
           data_valid => data_valid,
-          data_out => data_out,
+          data_out => data_bus,
           start_packet => start_packet,
           end_packet => end_packet,
           debug_out => debug_out
         );
+   parse:parser PORT map (
+          clk_read => clock,
+          clk_parse => clock_parse,
+			 reset=>reset,
+          data_valid => data_valid,
+          data_in => data_bus,
+          start_packet => start_packet,
+          end_packet => end_packet,
+          debug_output => debug_out_parser
+	);
 
    clock_process :process
    begin
@@ -45,6 +70,14 @@ BEGIN
 		wait for clock_period/2;
 		clock <= '1';
 		wait for clock_period/2;
+   end process;
+	
+   clock_parser_process :process
+   begin
+		clock_parse <= '0';
+		wait for clock_parser_period/2;
+		clock_parse <= '1';
+		wait for clock_parser_period/2;
    end process;
  
 
