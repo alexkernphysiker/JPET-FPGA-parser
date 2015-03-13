088606ee8a2d5c2f518890e287eeee5835d0acde
Rundel <rundel@wfais.uj>
2015-03-13 17:14:40 +0100

    packet_started variable's state is changed

--- packet_simulation/parser.vhd
+++ packet_simulation/parser.vhd
@@ -6,38 +6,44 @@ entity parser is
 	        clk_read : in  STD_LOGIC;
            clk_parse : in  STD_LOGIC;
            reset : in  STD_LOGIC;
-           data_valid : in  STD_LOGIC;
-			  data_in : in STD_LOGIC_VECTOR(7 downto 0);
            start_packet : in  STD_LOGIC;
            end_packet : in  STD_LOGIC;
-			  
+           data_valid : in  STD_LOGIC;
+			  data_in : in STD_LOGIC_VECTOR(7 downto 0);
 			  debug_output: out STD_LOGIC_VECTOR (127 downto 0)
-			  
 			);
 end parser;
 architecture Behavioral of parser is
 begin
 reading:process(clk_read)
 variable packet_started: boolean:=false;
-variable queue_started:boolean:=false;
-variable queue_counter:integer:=0;
-variable subqueue_started:boolean:=false;
-variable subqueue_counter:integer:=0;
 begin
-	if(rising_edge(clk_read))then
-		if(packet_started)then
-			case end_packet is
-				when '1' => packet_started:=false;
-				when others => packet_started:=true;
-			end case;
+	if(falling_edge(clk_read))then
+		debug_output(1)<=data_valid;
+		debug_output(4)<=start_packet;
+		debug_output(5)<=end_packet;
+		if packet_started then
+			debug_output(2)<='1';
+			if end_packet>'0' then
+				packet_started:=false;
+			end if;
 		else
-			case start_packet is
-				when '1' => packet_started:=true;
-				when others =>packet_started:=true;
-			end case;
+			debug_output(2)<='0';
+			if start_packet>'0' then
+				packet_started:=true;
+			end if;
 		end if;
 		if(packet_started)then
+			if(data_valid>'0')then
+				debug_output(3)<='1';
+			else
+				debug_output(3)<='0';
+			end if;
 		end if;
+		case packet_started is
+			when true => debug_output(0)<='1';
+			when others => debug_output(0)<='0';
+		end case;
 	end if;
 end process reading;
 end Behavioral;
