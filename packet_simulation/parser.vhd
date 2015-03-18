9652c90ced09b7cc1747fa0931504cd5661326c0
Rundel <rundel@wfais.uj>
2015-03-18 15:46:20 +0100

    debug output: there's a signal that correctly informs if there's a byte read from the packet

--- packet_simulation/parser.vhd
+++ packet_simulation/parser.vhd
@@ -18,32 +18,28 @@ begin
 reading:process(clk_read)
 variable packet_started: boolean:=false;
 begin
+	debug_output(1)<=data_valid;
+	debug_output(2)<=start_packet;
+	debug_output(3)<=end_packet;
 	if(falling_edge(clk_read))then
-		debug_output(1)<=data_valid;
-		debug_output(4)<=start_packet;
-		debug_output(5)<=end_packet;
 		if packet_started then
-			debug_output(2)<='1';
 			if end_packet>'0' then
 				packet_started:=false;
 			end if;
 		else
-			debug_output(2)<='0';
 			if start_packet>'0' then
 				packet_started:=true;
 			end if;
 		end if;
 		if(packet_started)then
 			if(data_valid>'0')then
-				debug_output(3)<='1';
+				debug_output(0)<='1';
 			else
-				debug_output(3)<='0';
+				debug_output(0)<='0';
 			end if;
+		else
+			debug_output(0)<='0';
 		end if;
-		case packet_started is
-			when true => debug_output(0)<='1';
-			when others => debug_output(0)<='0';
-		end case;
 	end if;
 end process reading;
 end Behavioral;
