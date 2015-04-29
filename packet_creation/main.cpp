69301ef64de7741957eed4ca46e664f5d9096ee6
alexkernphysiker <kernphysiker@openmailbox.org>
2015-04-29 14:20:54 +0200

    test upgrade

--- packet_creation/main.cpp
+++ packet_creation/main.cpp
@@ -17,14 +17,14 @@ int main(int arg_cnt,char**arg) {
 		shared_ptr<TDC> tdc1=make_shared<TDC>(0x7771,0);
 		for(numtype i=1;i<=5;i++)tdc1<<i;
 		shared_ptr<TDC> tdc2=make_shared<TDC>(0x7772,0);
-		for(numtype i=1;i<=5;i++)tdc2<<i;
+		for(numtype i=1;i<=3;i++)tdc2<<i;
 		alldetectors<<tdc1<<tdc2;
 		TDCTime timeconst(0xff,0xff,0xff,true);
 		for(numtype i=1;i<=5;i++)(*tdc1)[i]<<timeconst;
-		for(numtype i=1;i<=5;i++)(*tdc2)[i]<<timeconst;
+		for(numtype i=1;i<=3;i++)(*tdc2)[i]<<timeconst;
 		wrap<<alldetectors.getEvent(0x1d1d00);
 		wrap<<alldetectors.getEvent(0x1d1d01);
-		for(numtype i=1;i<=5;i++)(*tdc2)[i]<<timeconst;
+		for(numtype i=1;i<=3;i++)(*tdc2)[i]<<timeconst;
 		wrap<<alldetectors.getEvent(0x1d1d02);
 		for(numtype i=1;i<=5;i++)(*tdc1)[i]<<timeconst;
 		wrap<<alldetectors.getEvent(0x1d1d03);
